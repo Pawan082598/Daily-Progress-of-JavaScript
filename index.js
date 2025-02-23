50f1da1980930d786eca40be15711aa51b4e6dbb
Pawan082598 <pawan082598@gmail.com>
2025-02-23 23:58:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hello Everyone");
+
+//   Var
+var message;
+message="Hello";
+console.log(message);
+
+var num=30;
+num=38;  // reassigning the value of num 
+console.log(num);
+
+// Let
+
+let num1=54;
+num1=67;  // reassigning the value of num1
+console.log(num1);
+
+// Const
+const num2=90;
+ num2=100;  // cannot reassign the value of num2
+ console.log(num2);
+
+
