4e02041272a2a0e434c21ecdaeced7399c523ec9
Pawan082598 <pawan082598@gmail.com>
2025-02-23 23:59:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
-console.log("Hello Everyone");
+// Hoisting
 
-//   Var
-var message;
-message="Hello";
-console.log(message);
+// var num;
+// console.log(num);
+// var num = 20;//  output : undefined
 
-var num=30;
-num=38;  // reassigning the value of num 
-console.log(num);
-
-// Let
-
-let num1=54;
-num1=67;  // reassigning the value of num1
-console.log(num1);
-
-// Const
-const num2=90;
- num2=100;  // cannot reassign the value of num2
- console.log(num2);
+// num = 20;
+// console.log(num);//these type of syntax can only applicable in "var".
+// var num;
 
 
+// num1 = 34;
+// console.log(num1);// output : it shows error.
+// let num1;
+
+// Operators
+
+// 1. Arithmetic Operators
+  
+let a = 18;
+let b = 90;
+let c = (a+b);
+console.log(c);
+
+
+let x = 29;
+let y = -28;
+let z = (x-y);
+console.log(z);
+
+// Comparison Operators
+
+let p = 34;
+let q = 45;
+let r = p !==q;
+console.log(r);
+
+
+let m = 23;
+let n = "23";
+let o = m !==n;
+
+
+let s = 28;
+let t = 29;
+let u = s ===t;
+console.log(u);
+
+
+
+let d = 234566;
+let result = Math.floor(d/10);// Math.floor() rounds a number DOWN to the nearest integer.
+console.log(result);
+
+
+let g = 1234567// Math.ceil() rounds a number UP to the nearest integer.
+let result1 = Math.ceil(g/1000);
+console.log(result1);
