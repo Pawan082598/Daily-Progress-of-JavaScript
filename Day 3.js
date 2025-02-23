f1fd183275c4392e881e64ae4fc11e152562a8e6
Pawan082598 <pawan082598@gmail.com>
2025-02-24 02:09:21 +0530

    Add files via upload

---
+++ Day 3.js
@@ -0,0 +1,44 @@
+// post-increment operator
+let a = 20;
+console.log(a++, a);
+
+//pre-increment operator
+ let b = 35;
+ console.log(++b, b);
+
+ let c = 20;
+ console.log(++c, c, c++, ++c, c, c++,c++,++c , ++c);
+
+
+ // Decrement operators
+  
+ let d = 20;
+ console.log(d--, d, --d,--d,d--,d,d,d,--d,d--,d--);
+
+
+let e = 20;
+ console.log(++e,e,--e,e--,e--,e,e,e--,e++,++e,++e,e++,--e);
+
+
+ //Logical operators
+
+ let f = 15;
+ let g = 20;
+ console.log(f>g && f<g);
+
+
+ console.log(f===g && g == f);
+
+
+ console.log(f !=g && g != f);
+
+ console.log(f === g || f == g);
+
+
+//Ternary or Conditional operator
+
+// Condition ? value 1 : value 2
+let h = true;
+let i = false;
+
+( 5 === 5) ? 'h' : 'i';
